faa33b9c742e5fb4bff41da36593544f1960ad1d
gayathrik92 <47923788+gayathrik92@users.noreply.github.com>
2019-12-29 23:28:23 +0530

    Initial  Commit

---
+++ src/main/java/freshworks/com/springbooterstarter/datastore/CustomizeException.java
@@ -0,0 +1,10 @@
+package freshworks.com.springbooterstarter.datastore;
+
+public class CustomizeException extends Exception {
+
+	public CustomizeException(String string) {
+		// TODO Auto-generated constructor stub
+		System.out.println(string);
+	}
+
+}
